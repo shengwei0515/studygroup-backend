a0ef72a42595bac5c73460afd26e5d3d4337265b
YingSyuan <i102240622@gmail.com>
2022-09-14 12:11:00 +0800

    remove dead code and modify db init

--- db/postgres/postgres.go
+++ db/postgres/postgres.go
@@ -10,25 +10,21 @@ import (
 
 var db *gorm.DB
 
-const dbReconnectTimes = 5
-const dbReconnectSec = 1
-
 func InitWithRetry(dbUri, dbDriver string, reconnectTimes int, reconnectBounceSec time.Duration) {
 	var err interface{}
 	for i := 0; i < reconnectTimes; i += 1 {
-		err = Init(dbUri, dbDriver)
+		err = initDb(dbUri, dbDriver)
 		if err == nil {
-			break
+			return
 		}
 		log.Printf("DB Init error %s\nWait for %s second to reconnect DB", err, reconnectBounceSec)
 		time.Sleep(reconnectBounceSec)
 	}
-	if err != nil {
-		log.Panicf("DB Init error after retry %c time, error: %s", reconnectBounceSec, err)
-	}
+
+	log.Panicf("DB Init error after retry %c times, error: %s", reconnectBounceSec, err)
 }
 
-func Init(dbUri, dbDriver string) error {
+func initDb(dbUri, dbDriver string) error {
 	postgres, err := gorm.Open(dbDriver, dbUri)
 	if err != nil {
 		return fmt.Errorf("Connect to postgres failed with error: %s", err)
